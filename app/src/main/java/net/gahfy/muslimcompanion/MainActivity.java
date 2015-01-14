f631d25835450ff114367ea154f5326d689d8234
Gaëtan <gaetan.hfy@gmail.com>
2015-01-14 15:54:39 +0100

    Adding icons and adding first activity of the application

---
+++ app/src/main/java/net/gahfy/muslimcompanion/MainActivity.java
@@ -0,0 +1,10 @@
+package net.gahfy.muslimcompanion;
+
+import android.app.Activity;
+
+/**
+ * As its name says, this is the main Activity of the application
+ * @author Gahfy
+ */
+public class MainActivity extends Activity {
+}
