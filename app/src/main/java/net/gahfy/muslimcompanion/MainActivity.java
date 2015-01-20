b3573c06540789b79edd17568f67a9b5fc0de378
Gaëtan <gaetan.hfy@gmail.com>
2015-01-20 14:55:52 +0100

    Customization of style with ActionBar

--- app/src/main/java/net/gahfy/muslimcompanion/MainActivity.java
+++ app/src/main/java/net/gahfy/muslimcompanion/MainActivity.java
@@ -1,7 +1,8 @@
 package net.gahfy.muslimcompanion;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.analytics.Tracker;
 
@@ -12,7 +13,7 @@ import net.gahfy.muslimcompanion.fragment.CompassFragment;
  * As its name says, this is the main Activity of the application
  * @author Gahfy
  */
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends ActionBarActivity {
     /** The fragment that is currently attached to the Activity */
     AbstractFragment currentFragment;
 
@@ -23,6 +24,11 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 
         this.setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setTitle(R.string.app_name);
 
         // Get tracker.
         activityTracker = ((MuslimCompanionApplication) getApplication()).getTracker(
